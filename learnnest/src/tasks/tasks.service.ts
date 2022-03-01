03bc9da1580112b6f55802beb7f6f4bba14b8b77
tahamoeini <taha.moeini@gmail.com>
2022-03-01 20:39:47 +0000

    add validation to controller&add dtos

--- learnnest/src/tasks/tasks.service.ts
+++ learnnest/src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -35,7 +35,12 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find((task) => task.id === id);
+        const found = this.tasks.find((task) => task.id === id);
+
+        if (!found) {
+            throw new NotFoundException(`No Task with this Task ID of ${id}, Khodafez!(Some Persian bye)`);
+        }
+        return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -53,6 +58,7 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
+        const found = this.getTaskById(id);
         this.tasks = this.tasks.filter((task) => task.id !== id);
     }
 
