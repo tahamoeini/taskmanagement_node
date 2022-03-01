64e9e1ef51945adbc96ce9521ea9d64b1688e395
tahamoeini <taha.moeini@gmail.com>
2022-03-01 20:19:33 +0000

    search in status or strings of title&desc of Tasks

--- learnnest/src/tasks/tasks.service.ts
+++ learnnest/src/tasks/tasks.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { filter } from 'rxjs';
 
 @Injectable()
 export class TasksService {
@@ -9,6 +11,27 @@ export class TasksService {
 
     getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+        const { status, search } = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter((task) => task.status === status)
+        }
+        if (search) {
+            tasks = tasks.filter((task) => {
+                // .toLocaleLowerCase() is used for english can be remove for persian
+                if (task.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()) || task.description.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                    return true;
+                }
+                return false;
+            })
+        }
+
+        return tasks;
     }
 
     getTaskById(id: string): Task {
