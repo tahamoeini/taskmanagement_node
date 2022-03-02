f735225e77262ce6ae4dc400d7c6475980c86801
tahamoeini <taha.moeini@gmail.com>
2022-03-02 17:02:53 +0000

    migrate to database, first remove task.modules

--- learnnest/src/tasks/tasks.service.ts
+++ learnnest/src/tasks/tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
+import { Task, TaskStatus } from './task-status.enum';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
