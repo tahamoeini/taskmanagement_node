4f41e79e78aa5e737c3234e3880a5c414deb8ac2
tahamoeini <taha.moeini@gmail.com>
2022-03-02 17:09:18 +0000

    getTaskById become promise&syncronis base

--- learnnest/src/tasks/tasks.controller.ts
+++ learnnest/src/tasks/tasks.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
-import { Task, TaskStatus } from './task-status.enum';
+import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
--- learnnest/src/tasks/tasks.service.ts
+++ learnnest/src/tasks/tasks.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task-status.enum';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { filter } from 'rxjs';
+import { TasksRepository } from './tasks.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './dto/task.entity';
 
 @Injectable()
 export class TasksService {
+    constructor(@InjectRepository(TasksRepository) private tasksRepository: TasksRepository) { }
+
     private tasks: Task[] = [];
 
     getAllTasks(): Task[] {
@@ -34,8 +37,8 @@ export class TasksService {
         return tasks;
     }
 
-    getTaskById(id: string): Task {
-        const found = this.tasks.find((task) => task.id === id);
+    async getTaskById(id: string): Promise<Task> {
+        const found = await this.tasksRepository.findOne(id);
 
         if (!found) {
             throw new NotFoundException(`No Task with this Task ID of ${id}, Khodafez!(Some Persian bye)`);
